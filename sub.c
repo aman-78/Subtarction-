f656f4e12370ff7df6fa9393f4dd2227e540ff9a
Aman Verma <126274969+aman-78@users.noreply.github.com>
2023-02-25 15:46:29 +0530

    Add files via upload

---
+++ sub.c
@@ -0,0 +1,11 @@
+#include<stio.h>
+int main(){
+	int first,second,sub;
+	printf("Enter First Number:");
+	scanf("%d",&first);
+	printf("Enter Second Number:");
+	scanf("%d",&second);
+	sub=first-second;
+	printf("Subtraction of two number is:",sub);
+	return 0;
+}
